326b7f613827b423c3756fa60398edcadcdd1faf
Shivam Chaugule <114950843+ShivamC93@users.noreply.github.com>
2024-01-20 00:31:38 +0530

    Add files via upload

---
+++ recursion15.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class recursion15 {
+    //reverse given string
+    static String reverse(String s, int idx){
+        if(idx == s.length()) return "";
+
+        String smallAns = reverse(s, idx+1);
+
+        return smallAns + s.charAt(idx);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String s = sc.nextLine();
+
+        System.out.println(reverse(s, 0));
+    }
+}
